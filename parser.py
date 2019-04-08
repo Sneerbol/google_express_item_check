00b669570b1ad3d677767ed5a3f67e6bcc3238af
Sneerbol <romas.m@mail.ru>
2019-04-08 11:03:58 +0300

    added comments to code

--- parser.py
+++ parser.py
@@ -4,9 +4,10 @@ print ('____________________________________________________________')
 
 timer = 0
 kolvo = 40
-d = open('txt.txt')
+
+d = open('txt.txt') '''FILE WITH LINKS TO PRODUCTS IN GOOGLE EXPRESS '''
 line = d.readlines()
-name_of_file = "1.html"
+name_of_file = "1.html" """ CAN CHANGE TO ANY NAME + .html """
 url = ""
 
 
@@ -14,13 +15,13 @@ while timer < kolvo:
     timer+=1
     url = line[timer]
 
-    name_of_file = "1" + name_of_file
+    name_of_file = "1" + name_of_file ''' changing file name '''
     r = requests.get(url)
     with open(name_of_file, 'wb') as code:       
         code.write(r.content)
 
     
-    f=open(name_of_file,'r',encoding='utf-8') #
+    f=open(name_of_file,'r',encoding='utf-8') ''' Open file and find "SOLD OUT" if true product isn't in stock '''
     n = 0
     for s in f:
         i = s.find("SOLD OUT")
